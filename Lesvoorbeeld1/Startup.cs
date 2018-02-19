8f237a4d929bd36239c97848f5dca966e740a49c
Tomtom8434 <tomromm@hotmail.com>
2018-02-19 10:07:25 +0100

    Just some text added and a few extensions

--- Lesvoorbeeld1/Startup.cs
+++ Lesvoorbeeld1/Startup.cs
@@ -25,9 +25,12 @@ namespace Lesvoorbeeld1
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello darkness my old friend!");
             });
         }
     }
